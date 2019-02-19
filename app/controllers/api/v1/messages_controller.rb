98fc5161070026ff4d05bff468dac7930730245f
Alexander Dubovoy <ajdubovoy@gmail.com>
2019-02-19 14:43:53 +0100

    post

--- app/controllers/api/v1/messages_controller.rb
+++ app/controllers/api/v1/messages_controller.rb
@@ -15,11 +15,34 @@ class Api::V1::MessagesController < ApplicationController
   end
 
   def create
+    @message = Message.new(message_params);
+    @message.channel = @channel
+    @message.user = current_user
+    @results = {
+      id: @message.id,
+      author: @message.user.email.gsub(/@.*/, ''),
+      content: @message.content,
+      created_at: @message.created_at
+    }
+  if @message.save!
+    render json: @results, status: :created
+  else
+    render json: {
+      id: 100,
+      author: "admin",
+      content: "There's been an error",
+      created_at: Time.now
+    }
+  end
   end
 
   private
 
   def load_channel
     @channel = Channel.find_by(name: params[:channel_id])
+  end
+
+  def message_params
+    params.require(:message).permit(:content)
   end
 end
