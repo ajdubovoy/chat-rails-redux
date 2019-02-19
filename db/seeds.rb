41e903026cb3c0c8224ef7f487f8b965ca41f773
Alexander Dubovoy <ajdubovoy@gmail.com>
2019-02-19 11:29:35 +0100

    seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Message.destroy_all
+User.destroy_all
+Channel.destroy_all
+
+general = Channel.create(name: 'general')
+paris = Channel.create(name: 'paris')
+react = Channel.create(name: 'react')
+berlin = Channel.create(name: 'berlin')
+lunch = Channel.create(name: 'lunch')
+
+charlotte = User.create(email: "charlotte@lunch.com", password: "charlotte")
+peter = User.create(email: "peter@lunch.com", password: "peter")
+
+Message.create(user: charlotte, content: "NUMMER ACHT PIZZA NAPOLI", channel: lunch)
+Message.create(user: peter, channel: general, content: "I would like a pizza please, and I know this channel is general")
+Message.create(user: charlotte, content: "NUMMER ACHT PIZZA NAPOLI", channel: general)
+Message.create(user: charlotte, content: "NUMMER ACHT PIZZA FRENCH", channel: paris)
+Message.create(user: charlotte, content: "NUMMER ACHT PIZZA REACT", channel: react)
+Message.create(user: charlotte, content: "NUMMER ACHT PIZZA CURRYWURST", channel: berlin)
